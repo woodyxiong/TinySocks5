16b5e9cee6f556ba711cf45fdc7563270faaf7bd
WoodyXiong <zuosixiaoshenxian@gmail.com>
2020-11-13 02:45:42 +0800

    feat: 完成内网穿透连接数据库

--- tcprelay.py
+++ tcprelay.py
@@ -3,20 +3,35 @@
 from __future__ import absolute_import
 import socket
 import logging
-import time
 
 BUF_SIZE = 32 * 1024
 
 STAGE_INIT = 0  # 收到客户端的连接
 STAGE_CONNECTED_REMOTE = 1  # 连接socks5服务端
-STAGE_STREAM = 2  # 已连接上
+STAGE_SEND_HELLO_TO_REMOTE = 2  # 客户端-》服务端 050100
+STAGE_RECEIVE_HELLO_FROM_REMOTE = 3  # 已连接上 服务端-》客户端 0500
+STAGE_SEND_REQUEST_ADDR = 4  # 已发送需要连接的地址
+STAGE_CONNECTED = 5  # 已连接上
 STAGE_DESTROYED = -1
+
+SOCKS5_SERVER_IP = '172.18.12.70'
+SOCKS5_SERVER_PORT = 10080
+MYSQL_IP = '10.104.20.42'
+MYSQL_PORT = 3306
+
+# SOCKS5_SERVER_IP = '49.234.85.242'
+# SOCKS5_SERVER_PORT = 6666
+# MYSQL_IP = '49.234.85.242'
+# MYSQL_PORT = 3306
 
 # SOCKS command definition
 CMD_CONNECT = 1
 CMD_BIND = 2
 CMD_UDP_ASSOCIATE = 3
 
+
+# 1. 客户端-》服务端 050100
+# 2. 服务端-》客户端 0500
 
 class Tcprelay(object):
     def __init__(self, server, conn, loop, config, fd_to_handlers):
@@ -35,17 +50,22 @@ class Tcprelay(object):
 
         self._loop.clear_we(self._local_sock.fileno())
 
-        self._remote_sock = self.create_remote_sock('172.18.12.70')
+        self._remote_sock = self.create_remote_sock((SOCKS5_SERVER_IP, SOCKS5_SERVER_PORT))
         if self._remote_sock:
             self._loop.add(self._remote_sock, self)
-            self._stage = STAGE_CONNECTED_REMOTE
             self._fd_to_handlers[self._remote_sock.fileno()] = self
+        else:
+            logging.warning("连接remote_sock失败")
+            self.destroy()
 
     def handle_event(self, sock, event):
         if event == 2:
             # 远程连接成功
             self._loop._impl.clear_we_list(sock.fileno())
-            logging.info("远程socks5服务端已连接")
+            self._stage = STAGE_CONNECTED_REMOTE
+            logging.info("连接remote_sock成功")
+            if self.write_to_sock(b'\x05\x01\x00', self._remote_sock):
+                self._stage = STAGE_SEND_HELLO_TO_REMOTE
             return
         elif event == 3:
             # 远程连接失败
@@ -76,15 +96,8 @@ class Tcprelay(object):
                 return
         except(OSError, IOError) as e:
             logging.warning("接收数据时发生错误")
-        if self._stage == STAGE_INIT:
-            # 建立握手
-            self.handle_stage_init(data)
             return
-        if self._stage == STAGE_ADDR:
-            # 与remote_sock连接
-            self.handle_stage_addr(data)
-            return
-        if self._stage == STAGE_CONNECTING:
+        if self._stage == STAGE_CONNECTED:
             self.handle_stage_connecting(data)
             return
         else:
@@ -187,11 +200,48 @@ class Tcprelay(object):
         except Exception as e:
             logging.warning("接收remote_sock失败" + str(e))
         if data:
-            # 转发
-            self.write_to_sock(data, self._local_sock)
+            if self._stage == STAGE_SEND_HELLO_TO_REMOTE:
+                self.on_receive_remote_hello(data)
+                return
+            if self._stage == STAGE_SEND_REQUEST_ADDR:
+                self.on_judge_connected(data)
+                return
+            if self._stage == STAGE_CONNECTED:
+                self.write_to_sock(data, self._local_sock)
+                return
+
         else:
             # remote 断开连接
             self.destroy()
+
+    def on_judge_connected(self, data):
+        if len(data) < 2:
+            logging.error("socks连接数据库失败")
+            self.destroy()
+            return
+        if bytes(data[1]) != b'':
+            logging.error("socks连接数据库失败")
+            self.destroy()
+            return
+        self._stage = STAGE_CONNECTED
+        if len(data) > 10:
+            first_content_bytes = data[10:]
+            self.write_to_sock(first_content_bytes, self._local_sock)
+
+    def on_receive_remote_hello(self, data):
+        if data != b'\x05\x00':
+            logging.warning("非标准的socks5握手data=" + str(data))
+            self.destroy()
+            return
+        self._stage = STAGE_RECEIVE_HELLO_FROM_REMOTE
+        mysql_ip = MYSQL_IP
+        mysql_port = MYSQL_PORT
+        output = b'\x05\x01\x00\x01' \
+                 + bytes(map(int, mysql_ip.split('.'))) \
+                 + mysql_port.to_bytes(2, byteorder='big')
+
+        if self.write_to_sock(output, self._remote_sock):
+            self._stage = STAGE_SEND_REQUEST_ADDR
 
     # 关闭连接，并销毁
     def destroy(self):
